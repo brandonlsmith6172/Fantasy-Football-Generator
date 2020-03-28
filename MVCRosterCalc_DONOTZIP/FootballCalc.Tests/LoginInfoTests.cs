9025e9d6522843d21782aab3096a1c5c146a51f2
brandonlsmith6172 <60222336+brandonlsmith6172@users.noreply.github.com>
2020-03-28 18:36:25 -0400

    Football calculator with test cases added

---
+++ MVCRosterCalc_DONOTZIP/FootballCalc.Tests/LoginInfoTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace FootballCalc.Tests
+{
+    public class LoginInfoTests
+    {
+        [Fact]
+        public void CanChangeUserName()
+        {
+            //Arrange
+            //do code work Look on Pg 170 in MVC book
+
+            //Act
+            //do code work here
+
+            //Assert
+            //do code work here
+        }
+        [Fact]
+        public void CanChangePassword()
+        {
+            //Arrange
+            //do code work Look on Pg 170 in MVC book
+
+            //Act
+            //do code work here
+
+            //Assert
+            //do code work here
+        }
+    }
+}
---
+++ MVCRosterCalc_DONOTZIP/FootballCalc.Tests/PlayerInfoTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FootballCalc.Tests
+{
+    public class PlayerInfoTests
+    {
+        [Fact]
+        public void CanChangePlayerName()
+        {
+            //Arrange
+            //do code work Look on Pg 170 in MVC book
+
+            //Act
+            //do code work here
+
+            //Assert
+            //do code work here
+        }
+        [Fact]
+        public void CanChangePlayerPrice()
+        {
+            //Aarrange
+            //do code work here
+
+            //Act
+            //do code work here
+
+            //Assert
+            //do code work here
+        }
+
+    }
+    
+}
