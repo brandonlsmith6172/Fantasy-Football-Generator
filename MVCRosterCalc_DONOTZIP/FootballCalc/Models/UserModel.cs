b9f41343805996df3c4abaf1525ca5b2521f5349
brandonlsmith6172 <60222336+brandonlsmith6172@users.noreply.github.com>
2020-04-03 14:26:32 -0400

    Brandon registration commit

---
+++ MVCRosterCalc_DONOTZIP/FootballCalc/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballCalc.Models
+{
+    public class UserModel
+    {
+        public int Userid { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
