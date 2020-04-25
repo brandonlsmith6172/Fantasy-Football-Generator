afe8ac9e5b376fc969a420ad3ffed77338504862
brandonlsmith6172 <60222336+brandonlsmith6172@users.noreply.github.com>
2020-04-25 15:14:01 -0400

    Developer views added and layouts fixed

---
+++ MVCRosterCalc_DONOTZIP/FootballCalc/Controllers/DevelopersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballCalc.Controllers
+{
+    public class DevelopersController : Controller
+    {
+        public IActionResult Brandon()
+        {
+            return View();
+        }
+        public IActionResult Austin()
+        {
+            return View();
+        }
+        public IActionResult Anthony()
+        {
+            return View();
+        }
+        public IActionResult AJ()
+        {
+            return View();
+        }
+        public IActionResult Christopher()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVCRosterCalc_DONOTZIP/FootballCalc/Models/Developers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballCalc.Models
+{
+    public class Developers
+    {
+        public string name { get; set; }
+
+        
+    }
+}
